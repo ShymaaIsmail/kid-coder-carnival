1f9e1359799e2bb62123983886adbbb8134570c7
shymaaismail <shymaa.m.ismail@gmail.com>
2024-04-24 08:19:45 +0300

    working apis testing

--- kid-coder-carnival-api/kidcodercarnivalapi/kidcodercarnivalapi/views/auth_views.py
+++ kid-coder-carnival-api/kidcodercarnivalapi/kidcodercarnivalapi/views/auth_views.py
@@ -13,6 +13,7 @@ class UserView(BaseAPIView):
         super().__init__()
         self.user_service = UserService()
 
-    def get(self, request, user_id):
+    def get(self, request):
+        user_id = request.GET.get('user_id')
         user_data = self.user_service.get_user_data(user_id)
         return JsonResponse(user_data)
